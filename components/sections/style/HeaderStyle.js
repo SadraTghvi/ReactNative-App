d39e5d9dff6877ea4ff5e2f7b97ec51ecba55bcc
sadra <sadrataghavi1383@gmail.com>
2021-10-29 22:07:58 +0330

    styling for header section

--- components/sections/style/HeaderStyle.js
+++ components/sections/style/HeaderStyle.js
@@ -3,7 +3,12 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const HeaderStyle = StyleSheet.create({
     container: {
+        // backgroundColor: '#f00',
+        borderRadius: 10,
+
+        padding: 10,
         flexDirection: 'row',
+        alignSelf: 'center',
     },
 })
 
