dcfeec1501cca22b3d842e5c9e03cbc78144e284
sadra <sadrataghavi1383@gmail.com>
2021-10-29 22:07:48 +0330

    header section

--- components/sections/Header.js
+++ components/sections/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text } from 'react-native'
 
 import HeaderButton from '../layout/HeaderButton'
@@ -6,10 +6,24 @@ import HeaderButton from '../layout/HeaderButton'
 import HeaderStyle from './style/HeaderStyle'
 
 export default function Header() {
+    const [activeTab, setactiveTab] = useState('Delivery')
+
     return (
         <View style={HeaderStyle.container}>
-            <HeaderButton text='Devliery' />
-            <HeaderButton text='Pickup' />
+            <HeaderButton
+                text='Devliery'
+                btnColor={'black'}
+                textColor={'white'}
+                activeTab={activeTab}
+                setactiveTab={setactiveTab}
+            />
+            <HeaderButton
+                text='Pickup'
+                btnColor={'white'}
+                textColor={'black'}
+                activeTab={activeTab}
+                setactiveTab={setactiveTab}
+            />
         </View>
     )
 }
