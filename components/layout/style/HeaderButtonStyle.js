03c389bc936e2aa90dfed9bbabc54bfe37c74c4d
sadra <sadrataghavi1383@gmail.com>
2021-10-29 22:07:40 +0330

    external styling for buttons

---
+++ components/layout/style/HeaderButtonStyle.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    buttons: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+
+        borderRadius: 30,
+    },
+    text: {
+        fontSize: 15,
+        fontWeight: '900',
+    },
+})
