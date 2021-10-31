92979ed9185bac0e76c13c192d67e660ddd2040f
sadra <sadrataghavi1383@gmail.com>
2021-10-31 22:44:01 +0330

    styling for search bar

---
+++ components/layout/style/SearchBarStyle.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Platform } from 'react-native'
+
+export default StyleSheet.create({
+    searchView: {
+        marginTop: 15,
+        flexDirection: "row",
+    },
+    searchBar: {
+
+    },
+    searchBarIconLeft:{
+        marginLeft: 10
+    },
+    searchBarIconRight:{
+        flexDirection: "row",
+        marginRight: 8,
+        padding: 9,
+        backgroundColor: "white",
+        borderRadius: 30,
+        alignItems:"center",
+    }
+})
