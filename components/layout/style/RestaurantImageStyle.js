d392818f5ff88edfd8d906eec5b4dc95f4d6a0a7
sadra <sadrataghavi1383@gmail.com>
2021-11-02 23:10:49 +0330

    styling for it

---
+++ components/layout/style/RestaurantImageStyle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    RestaurantImage: {},
+    icon: {
+        position: 'absolute',
+        right: 20,
+        top: 20,
+    },
+})
