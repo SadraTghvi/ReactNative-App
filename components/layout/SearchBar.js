ade6414fe93c37be0d8664880893fc070c726588
sadra <sadrataghavi1383@gmail.com>
2021-11-05 01:00:27 +0330

    search bar functionality wrote

--- components/layout/SearchBar.js
+++ components/layout/SearchBar.js
@@ -1,51 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
-// icons 
-import Ionicons from "react-native-vector-icons/Ionicons"
+// icons
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
-// styling 
+// styling
 import SearchBarStyle from './style/SearchBarStyle'
 
-const SearchBar = () => {
+const SearchBar = ({ cityHandler }) => {
     return (
         <View style={SearchBarStyle.searchView}>
-            <GooglePlacesAutocomplete
-                placeholder='Search...'
-                styles={{
-                    textInput: {
-                        backgroundColor: '#eee',
-                        borderRadius: 20,
-                        fontWeight: '700',
-                        marginTop: 7,
-                    },
-                    textInputContainer: {
-                        backgroundColor: "#eee",
-                        borderRadius: 50,
-                        flexDirection: "row",
-                        alignItems: "center",
-                        marginRight: 10,
-                    }
-                }}
-                renderLeftButton={() => {
-                    return (
-                        <View style={SearchBarStyle.searchBarIconLeft}>
-                            <Ionicons name='location-sharp' size={24} />
-                        </View>
-                    )
-                }}
-                renderRightButton={() =>{
-                    return(
-                        <View style={SearchBarStyle.searchBarIconRight}>
-                        <AntDesign name="clockcircle" size={11}  style={{marginRight:6}} />
-                            <Text>Search</Text>
-                        </View>
-                    )
-                }}
-            />
+            <TextInput
+                placeholder='Search City...'
+                onChangeText={e => cityHandler(e.split(" ")[0])}
+                onSubmitEditing={e => console.log("submit")}
+                style={SearchBarStyle.searchBar}
+            ></TextInput>
+            <View style={SearchBarStyle.searchBarIconLeft}>
+                <Ionicons name='location-sharp' size={24} />
+            </View>
+            <View style={SearchBarStyle.searchBarIconRight}>
+                <AntDesign
+                    name='clockcircle'
+                    size={11}
+                    style={{ marginRight: 6 }}
+                />
+                <Text>Search</Text>
+            </View>
         </View>
     )
 }
