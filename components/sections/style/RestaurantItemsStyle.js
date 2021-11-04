531e0883b7bc5d1c56ce442c0a31ceb56ee3e2bd
sadra <sadrataghavi1383@gmail.com>
2021-11-05 01:03:34 +0330

    new stylinf for restaurant items

--- components/sections/style/RestaurantItemsStyle.js
+++ components/sections/style/RestaurantItemsStyle.js
@@ -3,4 +3,7 @@ import { View, Text, StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     Container: { marginTop: 10, padding: 15, backgroundColor: 'white' },
+    NoRestaurant: {
+        textAlign: "center"
+    }
 })
