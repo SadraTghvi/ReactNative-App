afaeda76602cbed1a4866c0de161560813f797b5
sadra <sadrataghavi1383@gmail.com>
2021-11-05 00:58:37 +0330

    set a default img if the restaurant doesnt have prev img

--- components/layout/RestaurantImage.js
+++ components/layout/RestaurantImage.js
@@ -11,12 +11,18 @@ const RestaurantImage = ({image}) => {
         <>
             <Image
                 source={{
-                    uri: image,
+                    uri:
+                        image ||
+                        'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg',
                 }}
                 style={{ width: '100%', height: 180 }}
             />
             <TouchableOpacity style={RestaurantImageStyle.icon}>
-                <MaterialCommunityIcons name={"heart-outline"} size={25} color={"#fff"} />
+                <MaterialCommunityIcons
+                    name={'heart-outline'}
+                    size={25}
+                    color={'#fff'}
+                />
             </TouchableOpacity>
         </>
     )
