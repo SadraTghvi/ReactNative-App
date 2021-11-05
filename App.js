415c4f8813c0d4b3a4b6769447817cee54068b51
sadra <sadrataghavi1383@gmail.com>
2021-11-05 22:55:46 +0330

    link restaurant comp for now to work on it for now

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+// screen 
 import Home from './components/screens/Home'
+import RestaurantDetail from './components/screens/RestaurantDetail'
 
 export default function App() {
-    return <Home />
+    return <RestaurantDetail />
 }
