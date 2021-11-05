34ff9b984cafc274795e392b5e530beab72e37ad
sadra <sadrataghavi1383@gmail.com>
2021-11-05 20:07:40 +0330

    styling for bottom icons layout

---
+++ components/layout/style/BottomIconsStyle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+    },
+    icons: {
+        marginBottom: 3,
+        alignSelf: 'center',
+    },
+})
