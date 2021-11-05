9f348958ebb54f8a0b68216b974c4029439b7ae8
sadra <sadrataghavi1383@gmail.com>
2021-11-05 20:07:10 +0330

    styling for bottom tabs section

---
+++ components/sections/style/BottomTabsStyle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        margin: 10,
+        marginHorizontal: 20,
+        justifyContent: "space-between",
+    },
+})
