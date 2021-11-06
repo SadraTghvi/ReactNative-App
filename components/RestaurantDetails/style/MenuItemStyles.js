5edaad7ef2baee1555c1e3e6dca7ae80fb512225
sadra <sadrataghavi1383@gmail.com>
2021-11-06 16:58:05 +0330

    stylin for menu item

---
+++ components/RestaurantDetails/style/MenuItemStyles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flexDirection:"row",
+        justifyContent:"space-between",
+        margin:20,
+        
+    },
+
+})
