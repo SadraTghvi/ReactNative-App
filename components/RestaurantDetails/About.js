afa45a6487e6451e73fe0fb1e32f4ea5af6086cf
sadra <sadrataghavi1383@gmail.com>
2021-11-06 18:44:51 +0330

    getting navigation param and setting its own datas

--- components/RestaurantDetails/About.js
+++ components/RestaurantDetails/About.js
@@ -6,26 +6,13 @@ import RestaurantImage from './RestaurantImage'
 // styling
 import AboutStyle from './style/AboutStyle'
 
-const getRestaurantInfo = {
-    name: 'Farmhouse klosaokdka',
-    image: 'http://toptenteh.com/files/images/items/632/Leon-Roof-tt.jpg',
-    price: '$$',
-    reviews: '1500',
-    rating: 4.5,
-    categories: [
-        { title: 'Thai' },
-        { title: 'Thai' },
-        { title: 'Thai' },
-        { title: 'dsad' },
-    ],
-}
-const { name, image, price, reviews, rating, categories } = getRestaurantInfo
-const formattedCategories = categories.map(cat => cat.title).join(' • ')
-const description = `${formattedCategories} ${
-    price ? ' • ' + price : ''
-} • 🎫 • ${rating} ⭐ (${reviews}+)`
+const About = ({ route }) => {
+    const { name, image, price, reviews, rating, categories } = route.params
+    const formattedCategories = categories.map(cat => cat.title).join(' • ')
+    const description = `${formattedCategories} ${
+        price ? ' • ' + price : ''
+    } • 🎫 • ${rating} ⭐ (${reviews}+)`
 
-const About = () => {
     return (
         <View>
             <RestaurantImage image={image} />
