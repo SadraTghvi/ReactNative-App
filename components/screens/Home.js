e19e023b6286699fca8f8ba98bb37194eda0ca15
sadra <sadrataghavi1383@gmail.com>
2021-11-06 18:43:38 +0330

    passing nacvigation to home screen

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { View, SafeAreaView, ScrollView } from 'react-native'
 import { Divider } from 'react-native-elements'
 
-// components 
+// components
 import Header from '../sections/Header'
 import SearchBar from '../layout/SearchBar'
 import Categories from '../sections/Categories'
@@ -14,44 +14,43 @@ import RestaurantItems, { localRestaurants } from '../sections/RestaurantItems'
 // style
 import GlobalStyle from '../style/GlobalStyle'
 
-
 // api key
 const YELP_API_KEY =
     'rUGupgMisTmBdIrTZGxMKOw2jv0jO2F4ppdCUG2JVhYkoTv2CSou4o96PDiWaVJvOTACH072H2VnH4iIzlk5xWybVntO6AZlYFi-jZ_GHrzJqu3PcdFbTo1sglOAYXYx'
 
-export default function Home() {
-      const [restaurantData, setRestaurantData] = useState(localRestaurants)
-      const [city, setCity] = useState('Hollywood')
-      const [activeTab, setactiveTab] = useState('Delivery')
+export default function Home({ navigation }) {
+    const [restaurantData, setRestaurantData] = useState(localRestaurants)
+    const [city, setCity] = useState('Hollywood')
+    const [activeTab, setactiveTab] = useState('Delivery')
 
-      const getRestaurantsFromYelp = () => {
-          const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`
 
-          const apiOptions = {
-              mode: 'no-cors',
-              headers: {
-                  Authorization: `Bearer ${YELP_API_KEY}`,
-              },
-          }
+        const apiOptions = {
+            mode: 'no-cors',
+            headers: {
+                Authorization: `Bearer ${YELP_API_KEY}`,
+            },
+        }
 
-          return fetch(yelpUrl, apiOptions)
-              .then(res => res.json())
-              .then(json =>
-                  setRestaurantData(
-                      json.businesses !== undefined
-                          ? json.businesses.filter(business =>
-                                business.transactions.includes(
-                                    activeTab.toLowerCase()
-                                )
-                            )
-                          : null
-                  )
-              )
-      }
+        return fetch(yelpUrl, apiOptions)
+            .then(res => res.json())
+            .then(json =>
+                setRestaurantData(
+                    json.businesses !== undefined
+                        ? json.businesses.filter(business =>
+                              business.transactions.includes(
+                                  activeTab.toLowerCase()
+                              )
+                          )
+                        : null
+                )
+            )
+    }
 
-      useEffect(() => {
-          getRestaurantsFromYelp()
-      }, [city, activeTab])
+    useEffect(() => {
+        getRestaurantsFromYelp()
+    }, [city, activeTab])
 
     return (
         <SafeAreaView style={GlobalStyle.droidSafeArea}>
@@ -61,7 +60,10 @@ export default function Home() {
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
-                <RestaurantItems restaurantData={restaurantData} />
+                <RestaurantItems
+                    restaurantData={restaurantData}
+                    navigation={navigation}
+                />
             </ScrollView>
             <Divider width={1} />
             <BottomTabs />
