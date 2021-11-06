4202f58d5c3c7acff05ad953449cd46160a9ee7e
sadra <sadrataghavi1383@gmail.com>
2021-11-06 16:57:28 +0330

    styling for food layouts

---
+++ components/layout/style/FoodInfoStyle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        width: 240,
+        justifyContent: 'space-evenly',
+    },
+    title: {
+        fontSize:19,
+        fontWeight:"600",
+    },
+})
