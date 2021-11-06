41d1865cf4dfd196584dad61715d6acd9efde25c
sadra <sadrataghavi1383@gmail.com>
2021-11-06 16:55:16 +0330

    linked 2 new sections to Main screen

--- components/RestaurantDetails/About.js
+++ components/RestaurantDetails/About.js
@@ -3,19 +3,33 @@ import { View, Text } from 'react-native'
 
 import RestaurantImage from './RestaurantImage'
 
-// styling 
+// styling
 import AboutStyle from './style/AboutStyle'
 
-const img = 'http://toptenteh.com/files/images/items/632/Leon-Roof-tt.jpg'
-
-const title = 'Farmhouse klosaokdka'
-const description = 'thai 0 dsad'
+const getRestaurantInfo = {
+    name: 'Farmhouse klosaokdka',
+    image: 'http://toptenteh.com/files/images/items/632/Leon-Roof-tt.jpg',
+    price: '$$',
+    reviews: '1500',
+    rating: 4.5,
+    categories: [
+        { title: 'Thai' },
+        { title: 'Thai' },
+        { title: 'Thai' },
+        { title: 'dsad' },
+    ],
+}
+const { name, image, price, reviews, rating, categories } = getRestaurantInfo
+const formattedCategories = categories.map(cat => cat.title).join(' • ')
+const description = `${formattedCategories} ${
+    price ? ' • ' + price : ''
+} • 🎫 • ${rating} ⭐ (${reviews}+)`
 
 const About = () => {
     return (
         <View>
-            <RestaurantImage image={img} />
-            <Text style={AboutStyle.title}> {title} </Text>
+            <RestaurantImage image={image} />
+            <Text style={AboutStyle.title}> {name} </Text>
             <Text style={AboutStyle.description}> {description} </Text>
         </View>
     )
