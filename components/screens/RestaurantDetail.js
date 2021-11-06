4c560081a8dab094b4533b6773cd2666b90aa6e3
sadra <sadrataghavi1383@gmail.com>
2021-11-06 16:55:26 +0330

    linked 2 new sections to Main screen

--- components/screens/RestaurantDetail.js
+++ components/screens/RestaurantDetail.js
@@ -5,12 +5,14 @@ import { Divider } from 'react-native-elements'
 
 // components
 import About from '../RestaurantDetails/About'
+import MenuItems from '../RestaurantDetails/MenuItems'
 
 const RestaurantDetail = () => {
     return (
         <View>
             <About />
             <Divider width={1.8} style={{ marginVertical: 20 }} />
+            <MenuItems />
         </View>
     )
 }
