80c018825333044a8407a5d6d692833562c29973
sadra <sadrataghavi1383@gmail.com>
2021-11-11 15:08:56 +0330

    finished view card styling for now

---
+++ components/RestaurantDetails/style/ViewCartStyle.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 320,
+        zIndex: 999,
+    },
+    wrapper: {
+        width: 300,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30,
+        padding: 13,
+        position: 'relative',
+    },
+    text: {
+        fontSize: 20,
+        color: 'white',
+    },
+})
