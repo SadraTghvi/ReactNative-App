23dfac87a4ca49d2aac1f403435bd7922fcb3184
sadra <sadrataghavi1383@gmail.com>
2021-11-11 15:13:02 +0330

    added view cart to main comp

--- components/screens/RestaurantDetail.js
+++ components/screens/RestaurantDetail.js
@@ -6,10 +6,12 @@ import { Divider } from 'react-native-elements'
 // components
 import About from '../RestaurantDetails/About'
 import MenuItems from '../RestaurantDetails/MenuItems'
+import ViewCart from '../RestaurantDetails/ViewCart'
 
-const RestaurantDetail = ({ route }) => {
+const RestaurantDetail = ({ route, navigation }) => {
     return (
         <View>
+            <ViewCart />
             <About route={route} />
             <Divider width={1.8} style={{ marginVertical: 20 }} />
             <MenuItems />
