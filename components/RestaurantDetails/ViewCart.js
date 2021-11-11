a1e9af4da22af9af5e0f9d32d58ae01a7b646648
sadra <sadrataghavi1383@gmail.com>
2021-11-12 00:50:29 +0330

    getting redux store

--- components/RestaurantDetails/ViewCart.js
+++ components/RestaurantDetails/ViewCart.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+
+// redux
+import { useSelector } from 'react-redux'
 
 import ViewCartStyle from './style/ViewCartStyle'
 
 const ViewCart = () => {
+    const items = useSelector(state => state.CardReducer.items)
+    const totalPrice = items
+        .map(item => Number(item.price.replace('$', '')))
+        .reduce((prev, next) => prev + next, 0)
+
+    const totalPriceUSD = totalPrice
+        ? totalPrice.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+          })
+        : 0
+
+    console.log(totalPrice)
+
     return (
-        <View style={ViewCartStyle.container}>
-            <TouchableOpacity style={ViewCartStyle.wrapper}>
-                <Text style={ViewCartStyle.text}>View Cart 🛒</Text>
-            </TouchableOpacity>
-        </View>
+        <>
+            {totalPrice ? (
+                <View style={ViewCartStyle.container}>
+                    <TouchableOpacity style={ViewCartStyle.wrapper}>
+                        <Text style={ViewCartStyle.text}>View Cart 🛒</Text>
+                    </TouchableOpacity>
+                </View>
+            ) : (
+                <></>
+            )}
+        </>
     )
 }
 
