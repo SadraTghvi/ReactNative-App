81e631f830a63cefc6fe3046db1fc28749b804ea
sadra <sadrataghavi1383@gmail.com>
2021-11-11 20:33:37 +0330

    bottom increased

--- components/RestaurantDetails/style/ViewCartStyle.js
+++ components/RestaurantDetails/style/ViewCartStyle.js
@@ -7,7 +7,7 @@ export default StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
-        bottom: 320,
+        bottom: 360,
         zIndex: 999,
     },
     wrapper: {
